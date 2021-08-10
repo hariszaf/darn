da7a499ed5b5c403249363e81f7e1be4b5ca9081
hariszaf <haris-zaf@hcmr.gr>
2021-08-10 12:12:12 +0100

    remove estuarine samples not used

--- use_cases/estuarine/SRR11808870/vigo_d_10/3.correct_by_BayesHammer/SRR1808870/run_spades.sh
+++
@@ -1,7 +0,0 @@
-set -e
-true
-/home/tools/SPAdes/SPAdes-3.14.0/bin/spades-hammer /mnt/analysis/vigo_d_10/3.correct_by_BayesHammer/SRR1808870/corrected/configs/config.info
-/home/tools/OBI/OBI-env/bin/python /home/tools/SPAdes/SPAdes-3.14.0/share/spades/spades_pipeline/scripts/compress_all.py --input_file /mnt/analysis/vigo_d_10/3.correct_by_BayesHammer/SRR1808870/corrected/corrected.yaml --ext_python_modules_home /home/tools/SPAdes/SPAdes-3.14.0/share/spades --max_threads 20 --output_dir /mnt/analysis/vigo_d_10/3.correct_by_BayesHammer/SRR1808870/corrected
-true
-/home/tools/OBI/OBI-env/bin/python /home/tools/SPAdes/SPAdes-3.14.0/share/spades/spades_pipeline/scripts/breaking_scaffolds_script.py --result_scaffolds_filename /mnt/analysis/vigo_d_10/3.correct_by_BayesHammer/SRR1808870/scaffolds.fasta --misc_dir /mnt/analysis/vigo_d_10/3.correct_by_BayesHammer/SRR1808870/misc --threshold_for_breaking_scaffolds 3
-true
